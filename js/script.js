e3f576811ddc4322d49a47837562d6df09440b88
Florian Miller <flokomi@web.de>
2023-05-05 20:12:26 +0200

    added smooth scrolling with js

--- js/script.js
+++ js/script.js
@@ -14,6 +14,34 @@ btnNavEL.addEventListener("click", () => {
 });
 
 ///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to ther links
+    if ((href != "#") & href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEL.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
